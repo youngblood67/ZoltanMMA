e358dec717c763388612354bff9efc504681b2d8
Philippe Schaeffer <philippeschaeffer67@gmail.Com>
2026-03-04 15:23:20 +0100

    Configuration DI couche Infrastructure avec ajout du nugget Microsoft.Extensions.Http

--- src/ZoltanMMA.Infrastructure/ExternalApis/ApiSportsMma/InfrastructureDependencyInjection.cs
+++ src/ZoltanMMA.Infrastructure/ExternalApis/ApiSportsMma/InfrastructureDependencyInjection.cs
@@ -1,6 +1,23 @@
-﻿namespace ZoltanMMA.Infrastructure.ExternalApis.ApiSportsMma
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace ZoltanMMA.Infrastructure.ExternalApis.ApiSportsMma
 {
-    public  class InfrastructureDependencyInjection
+    public static class InfrastructureDependencyInjection
     {
+        public static IServiceCollection AddInfrastructure(
+            this IServiceCollection services,
+            IConfiguration configuration)
+        {
+            services.AddHttpClient<IApiSportsClient, ApiSportsClient>(client =>
+            {
+                client.BaseAddress = new Uri("https://v1.mma.api-sports.io/");
+                client.DefaultRequestHeaders.Add(
+                    "x-apisports-key",
+                    configuration["ApiSports:Key"]);
+            });
+
+            return services;
+        }
     }
 }
